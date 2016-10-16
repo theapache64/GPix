5187f8497db1353f0f30b975767a0d577463279e
theapache64@charlie <theapache64@gmail.com>
2016-10-16 10:07:50 +0530

    Need to build images table

--- server_primary/src/com/theah64/gpix/server/primary/servlets/GPixServlet.java
+++ server_primary/src/com/theah64/gpix/server/primary/servlets/GPixServlet.java
@@ -1,11 +1,25 @@
 package com.theah64.gpix.server.primary.servlets;
 
+import com.theah64.gpix.server.primary.core.GPix;
+import com.theah64.gpix.server.primary.core.Image;
 import com.theah64.gpix.server.primary.database.tables.Requests;
+import com.theah64.gpix.server.primary.database.tables.Users;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.servlet.annotation.WebServlet;
+import java.util.List;
 
 /**
  * Created by theapache64 on 15/10/16.
  */
+@WebServlet(urlPatterns = {AdvancedBaseServlet.VERSION_CODE + "/gpix"})
 public class GPixServlet extends AdvancedBaseServlet {
+
+    private static final String KEY_IMAGE_URL = "image_url";
+    private static final String KEY_THUMB_URL = "thumb_url";
+    private static final String KEY_HEIGHT = "height";
+    private static final String KEY_WIDTH = "width";
 
     @Override
     protected boolean isSecureServlet() {
@@ -19,6 +33,19 @@ public class GPixServlet extends AdvancedBaseServlet {
 
     @Override
     protected void doAdvancedPost() throws Exception {
-        
+        final String keyword = getStringParameter(Requests.COLUMN_KEYWORD);
+        final List<Image> images = GPix.getInstance().search(keyword);
+
+        final JSONArray jaImages = new JSONArray();
+
+        //Looping through each images
+        for (final Image image : images) {
+
+            final JSONObject joImage = new JSONObject();
+
+            joImage.put(KEY_IMAGE_URL, )
+
+        }
+
     }
 }
