2e30c2d35f49adf26015340d3ce71bf1ae52c6ef
theapache64@charlie <theapache64@gmail.com>
2016-10-19 09:51:49 +0530

    Added request adding method

--- server_primary/src/com/theah64/gpix/server/primary/database/tables/Requests.java
+++ server_primary/src/com/theah64/gpix/server/primary/database/tables/Requests.java
@@ -1,6 +1,11 @@
 package com.theah64.gpix.server.primary.database.tables;
 
+import com.theah64.gpix.server.primary.database.Connection;
 import com.theah64.gpix.server.primary.models.Request;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by theapache64 on 16/10/16.
@@ -20,9 +25,36 @@ public class Requests extends BaseTable<Request> {
     }
 
     @Override
-    public String addv3(Request newInstance) {
+    public String addv3(Request request) {
         String newRequestId = null;
         final String query = "INSERT INTO requests (user_id, keyword,limit) VALUES (?,?,?);";
+        final java.sql.Connection con = Connection.getConnection();
+        try {
+            final PreparedStatement ps = con.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, request.getUserId());
+            ps.setString(2, request.getKeyword());
+            ps.setInt(3, request.getLimit());
+
+            if (ps.executeUpdate() == 1) {
+                final ResultSet rs = ps.getGeneratedKeys();
+                if (rs.first()) {
+                    newRequestId = rs.getString(1);
+                }
+                rs.close();
+            }
+
+            ps.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return newRequestId;
     }
 }
