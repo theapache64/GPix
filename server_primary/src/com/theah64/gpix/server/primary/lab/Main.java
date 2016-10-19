77e9f33cb26df0d33e58f274325d7732552fad09
theapache64@charlie <theapache64@gmail.com>
2016-10-20 01:07:01 +0530

    Playing with pool

--- server_primary/src/com/theah64/gpix/server/primary/lab/Main.java
+++ server_primary/src/com/theah64/gpix/server/primary/lab/Main.java
@@ -1,15 +1,19 @@
 package com.theah64.gpix.server.primary.lab;
 
 import com.theah64.gpix.server.primary.core.GPix;
+import com.theah64.gpix.server.primary.database.Connection;
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * Created by theapache64 on 19/10/16,8:12 AM.
  */
 public class Main {
     public static void main(String[] args) throws GPix.GPixException, JSONException, IOException {
+
+
 
     }
 
--- server_primary/src/com/theah64/gpix/server/primary/servlets/TestServlet.java
+++ server_primary/src/com/theah64/gpix/server/primary/servlets/TestServlet.java
@@ -1,7 +1,37 @@
 package com.theah64.gpix.server.primary.servlets;
 
+import com.theah64.gpix.server.primary.database.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
 /**
  * Created by theapache64 on 19/10/16.
  */
-public class TestServlet {
+@WebServlet(urlPatterns = {"/test"})
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        for (int i = 0; i < 100000; i++) {
+
+            System.out.println("--------------------------");
+
+            final java.sql.Connection con = Connection.getConnection();
+            System.out.println("Opened connection " + i);
+            try {
+                con.close();
+                System.out.println("Closed connection " + i);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            System.out.println("--------------------------");
+
+        }
+
+    }
 }
