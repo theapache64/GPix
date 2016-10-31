a0e4e113cc48a0682c380a253ae9180872206d98
theapache64-bravo <theapache64@gmail.com>
2016-10-31 19:38:10 +0530

    Added VCF root to AS

--- android_client/app/src/main/java/com/theah64/gpix/ui/MainActivity.java
+++ android_client/app/src/main/java/com/theah64/gpix/ui/MainActivity.java
@@ -1,13 +1,9 @@
 package com.theah64.gpix.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
