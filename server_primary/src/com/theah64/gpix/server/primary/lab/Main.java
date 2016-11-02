db916073fa41212d3989a81be36cc64a57f98a72
theapache64-bravo <theapache64@gmail.com>
2016-11-02 22:53:29 +0530

    main cleaned

--- server_primary/src/com/theah64/gpix/server/primary/lab/Main.java
+++ server_primary/src/com/theah64/gpix/server/primary/lab/Main.java
@@ -11,10 +11,7 @@ import java.sql.SQLException;
  * Created by theapache64 on 19/10/16,8:12 AM.
  */
 public class Main {
-    public static void main(String[] args) throws GPix.GPixException, JSONException, IOException {
-
-
-
+    public static void main(String[] args) {
     }
 
 }
