afedb6d0211be52abb4d12b9e861d963f5946f84
theapache64-bravo <theapache64@gmail.com>
2016-11-08 08:13:56 +0530

    Added method to save bitmap

--- android_client/app/src/main/java/com/theah64/gpix/util/BitmapSaver.java
+++ android_client/app/src/main/java/com/theah64/gpix/util/BitmapSaver.java
@@ -3,14 +3,34 @@ package com.theah64.gpix.util;
 import android.graphics.Bitmap;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by theapache64 on 8/11/16.
  */
 
 public class BitmapSaver {
+
     public static String save(File folder, Bitmap loadedBitmap) {
-        //TODO:
+        FileOutputStream fos = null;
+        final String imagePath = folder + File.separator + RandomString.getRandomFilename(10, ".jpg");
+        try {
+            fos = new FileOutputStream(imagePath);
+            loadedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            return imagePath;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.flush();
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return null;
     }
 }
