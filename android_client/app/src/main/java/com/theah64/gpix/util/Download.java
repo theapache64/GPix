91ab91247857ed7dfb83f9d0e163b53dac33b4e5
theapache64-bravo <theapache64@gmail.com>
2016-11-08 08:22:50 +0530

    Download layout finished

---
+++ android_client/app/src/main/java/com/theah64/gpix/util/Download.java
@@ -0,0 +1,31 @@
+package com.theah64.gpix.util;
+
+import java.util.List;
+
+/**
+ * Created by theapache64 on 8/11/16.
+ */
+
+public class Download {
+
+    private final List<String> urls;
+    private final Callback callback;
+
+    public Download(List<String> urls, Callback callback) {
+        this.urls = urls;
+        this.callback = callback;
+    }
+
+    public void start() {
+        callback.onStart();
+
+    }
+
+    public interface Callback {
+        void onStart();
+
+        void onProgress(int perc);
+
+        void onFinish();
+    }
+}
